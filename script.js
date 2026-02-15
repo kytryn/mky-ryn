aec02c34443d68dc516dce8680d26c3c5df8c0e8
kytryn <borinagacatrynmaec@gmail.com>
2026-02-15 10:13:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -116,24 +116,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if(frameNumber > 1000 && frameNumber < 1250){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-            context.fillText("to be alive, and to get to spend this life with you", canvas.width/2, canvas.height/2);
+            context.fillText("to be alive, and to get to spend this life with you is so incredibly unfathomably unlikely", canvas.width/2, canvas.height/2);
             opacity += 0.01;
         }
         if(frameNumber >= 1250 && frameNumber < 1500){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-            context.fillText("to be alive, and to get to spend this life with you", canvas.width/2, canvas.height/2);
+            context.fillText("to be alive, and to get to spend this life with you is so incredibly unfathomably unlikely", canvas.width/2, canvas.height/2);
             opacity -= 0.01;
         }
         if(frameNumber == 1500) opacity = 0;
 
         if(frameNumber > 1500 && frameNumber < 1750){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-            context.fillText("is so incredibly, unfathomably unlikely", canvas.width/2, canvas.height/2);
+            context.fillText("and yet here I am to get the impossible chance to get to know and be with you", canvas.width/2, canvas.height/2);
             opacity += 0.01;
         }
         if(frameNumber >= 1750 && frameNumber < 2000){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-            context.fillText("is so incredibly, unfathomably unlikely", canvas.width/2, canvas.height/2);
+            context.fillText("and yet here I am to get the impossible chance to get to know and be with you", canvas.width/2, canvas.height/2);
             opacity -= 0.01;
         }
         if(frameNumber == 2000) opacity = 0;
@@ -141,18 +141,18 @@ document.addEventListener("DOMContentLoaded", function () {
         if(frameNumber > 2000 && frameNumber < 2250){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
             if(window.innerWidth < 600) {
-                drawTextWithLineBreaks(["and yet here I am to get the impossible", "chance to get to know and be with you"], canvas.width/2, canvas.height/2, fontSize, lineHeight);
+                drawTextWithLineBreaks(["I’ll be your shield and your shoulder"], canvas.width/2, canvas.height/2, fontSize, lineHeight);
             } else {
-                context.fillText("and yet here I am to get the impossible chance to get to know and be with you", canvas.width/2, canvas.height/2);
+                context.fillText("I’ll be your shield and your shoulder", canvas.width/2, canvas.height/2);
             }
             opacity += 0.01;
         }
         if(frameNumber >= 2250 && frameNumber < 2500){
             context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
             if(window.innerWidth < 600) {
-                drawTextWithLineBreaks(["and yet here I am to get the impossible", "chance to get to know and be with you"], canvas.width/2, canvas.height/2, fontSize, lineHeight);
+                drawTextWithLineBreaks(["I’ll stay with u thru every storm keeping you safe and close"], canvas.width/2, canvas.height/2, fontSize, lineHeight);
             } else {
-                context.fillText("and yet here I am to get the impossible chance to get to know and be with you", canvas.width/2, canvas.height/2);
+                context.fillText("I’ll stay with u thru every storm keeping you safe and close", canvas.width/2, canvas.height/2);
             }
             opacity -= 0.01;
         }
